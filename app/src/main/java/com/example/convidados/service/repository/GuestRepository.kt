ee44c49e03091358132439790adfe4888493249e
lucasnardelli <lucasnardellisaar@gmail.com>
2020-08-24 10:32:32 -0300

    Implementando o padrão Singleton

--- app/src/main/java/com/example/convidados/service/repository/GuestRepository.kt
+++ app/src/main/java/com/example/convidados/service/repository/GuestRepository.kt
@@ -1,31 +1,48 @@
 package com.example.convidados.service.repository
 
+import android.content.Context
 import com.example.convidados.service.model.GuestModel
 
-class GuestRepository{
-    fun getAll (): List<GuestModel>{
+class GuestRepository private constructor(context: Context) {
+
+    private var mGuestDataBaseHelper: GuestDataBaseHelper = GuestDataBaseHelper(context)
+
+    companion object {
+        private lateinit var repository: GuestRepository
+
+        fun getInstance(context: Context) : GuestRepository {
+            if(!::repository.isInitialized) {
+                repository = GuestRepository(context)
+            }
+            return repository
+        }
+    }
+
+    fun save(guest: GuestModel) {
+        mGuestDataBaseHelper.writableDatabase
+    }
+
+
+    fun getAll(): List<GuestModel> {
         val list: MutableList<GuestModel> = ArrayList()
         return list
     }
 
-    fun getPresent (): List<GuestModel>{
+    fun getPresent(): List<GuestModel> {
         val list: MutableList<GuestModel> = ArrayList()
         return list
     }
 
-    fun getAbsent (): List<GuestModel>{
+    fun getAbsent(): List<GuestModel> {
         val list: MutableList<GuestModel> = ArrayList()
         return list
     }
 
     // CRUD - Create, Read, Update, Delete
 
-    fun save(guest: GuestModel){
+    fun update(guest: GuestModel) {
     }
 
-    fun update(guest: GuestModel){
-    }
-
-    fun delete(guest: GuestModel){
+    fun delete(guest: GuestModel) {
     }
 }
